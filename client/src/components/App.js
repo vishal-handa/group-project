b30cf666c2ba94b540e531d10d8566ac48f5898c
Joni Sadler <joni.anne.sadler@gmail.com>
2021-02-06 15:01:28 -0500

    React router setup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import styled from "styled-components";
 
 function App() {
   const [bacon, setBacon] = useState(null);
@@ -9,7 +11,38 @@ function App() {
       .then(data => setBacon(data));
   }, []);
 
-  return <div>{bacon ? bacon : `...where's my stuff?...`}</div>;
+  // return <div>{bacon ? bacon : `...where's my stuff?...`}</div>;
+
+  return (
+    <BrowserRouter>
+      <Wrapper>
+        <Switch>
+
+          <Route exact path="/">
+            Home
+          </Route>
+
+          <Route exact path="/product/:id">
+            Individual product
+          </Route>
+
+          <Route exact path="/:category">
+            Category
+          </Route>
+
+          <Route exact path="/company/:companyId">
+            Company
+          </Route>
+
+        </Switch>
+        <Cart />
+      </Wrapper>
+    </BrowserRouter>
+  )
 }
+
+const Wrapper = styled.div` 
+  display: flex;
+`;
 
 export default App;
