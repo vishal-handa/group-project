7342a971ba3b290a27f20e4167db39345b94b155
“Winnie <chanwinnie.wc@gmail.com>
2021-02-07 18:44:47 -0500

    added state to homepage

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,20 +2,25 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import styled from "styled-components";
 import { useDispatch, useSelector } from 'react-redux';
-import { receiveCategories } from '../actions';
+import { receiveCategories, receiveCompanies } from '../actions';
 import Cart from "./Cart";
 import Menu from "./Menu";
 
 const App = () => {
   const dispatch = useDispatch();
-  const theCategories = useSelector(state=>state);
-  console.log(theCategories);
+  const homePageState = useSelector(state=>state);
+  console.log(homePageState);
   useEffect(() => {
     fetch('/categories')
       .then(res=>res.json())
       .then(res=>dispatch(receiveCategories(res.data)))
   }, []);
 
+  useEffect(() => {
+    fetch("/companies")
+      .then((res) => res.json())
+      .then((data) => dispatch(receiveCompanies(data.data)))
+  }, [])
 
   return (
     <BrowserRouter>
