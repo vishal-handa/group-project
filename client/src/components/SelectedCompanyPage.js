805810b5b5f56dc9ec48463f1d54938e0821297c
ChanWinnie <chanwinnie.wc@gmail.com>
2021-02-08 02:34:02 -0500

    added smallitembox to selected company page

--- client/src/components/SelectedCompanyPage.js
+++ client/src/components/SelectedCompanyPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import CompanyProduct from "./CompanyProduct";
+import ItemContainerSmall from "./ItemContainerSmall";
 
 const SelectedCompanyPage = () => {
 
@@ -7,7 +7,7 @@ const SelectedCompanyPage = () => {
         <div>
             Page once company has been selected from dropdown.
             Fetch and map over items from this company and display.
-            <CompanyProduct />
+            <ItemContainerSmall />
         </div>
     )
 }
