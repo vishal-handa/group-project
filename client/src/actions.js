7030498ce8471e0136e69ebab891e59794ecd4c8
ChanWinnie <chanwinnie.wc@gmail.com>
2021-02-14 09:25:18 -0500

    quantity change added to actions

--- client/src/actions.js
+++ client/src/actions.js
@@ -1,29 +1,35 @@
-export const receiveCategories=(categories)=>({
-    type: "RECEIVE_CATEGORIES",
-    categories,
+export const receiveCategories = (categories) => ({
+  type: "RECEIVE_CATEGORIES",
+  categories,
 });
 
-export const receiveItems=(items)=>({
-    type: "RECEIVE_ITEMS",
-    items,
+export const receiveItems = (items) => ({
+  type: "RECEIVE_ITEMS",
+  items,
 });
 
 export const receiveCompanies = (companies) => ({
-    type: "RECEIVE_COMPANIES",
-    companies,
+  type: "RECEIVE_COMPANIES",
+  companies,
 });
 
 export const addToCart = (item) => ({
-    type: "ADD_TO_CART",
-    item,
+  type: "ADD_TO_CART",
+  item,
 });
 
 export const removeFromCart = (item) => ({
-    type: "REMOVE_FROM_CART",
-    item,
+  type: "REMOVE_FROM_CART",
+  item,
 });
 
 export const clearCart = (item) => ({
-    type: "CLEAR_CART",
-    item,
+  type: "CLEAR_CART",
+  item,
+});
+
+export const changeCartQuantity = (item, quantityInCart) => ({
+  type: "CHANGE_CART_QUANTITY",
+  item,
+  quantityInCart,
 });
