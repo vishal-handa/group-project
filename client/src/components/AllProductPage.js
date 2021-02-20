b1f12033cebfe13baaf28a1055ff082f93e439c4
ChanWinnie <chanwinnie.wc@gmail.com>
2021-02-19 20:47:38 -0500

    banners changed

--- client/src/components/AllProductPage.js
+++ client/src/components/AllProductPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import ProductsPage from "./ProductsPage";
+import AllProductsImage from "./banner-images/allproducts-background.jpg";
 
 const AllProductPage = () => {
   const allTheProducts = useSelector((state) => state.items.items);
@@ -11,7 +12,13 @@ const AllProductPage = () => {
     return <div>Loading</div>;
   }
 
-  return <ProductsPage products={allTheProducts} bannerText={"All Products"} />;
+  return (
+    <ProductsPage
+      products={allTheProducts}
+      bannerText={"All Products"}
+      bannerImage={AllProductsImage}
+    />
+  );
 };
 
 export default AllProductPage;
